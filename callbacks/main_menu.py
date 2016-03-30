5853251fdb6afe71b82a6f9265346f92058aa366
huweius <huwilly.sh@gmail.com>
2016-03-30 15:50:24 -0400

    Initial Commit

---
+++ callbacks/main_menu.py
@@ -0,0 +1,9 @@
+def camera_control_callback(button):
+    mainbutton.parent.parent
+    print 'Camera Control is pressed'
+
+def movement_control_callback(button):
+    print 'Movement Control is pressed'
+
+def setting_callback(button):
+    print 'Setting is pressed'
