2a462a7dfedd138996326b203cb7a1f70988ebe5
huweius <huwilly.sh@gmail.com>
2016-03-30 16:06:11 -0400

    Initial Commit

--- callbacks/main_menu.py
+++ callbacks/main_menu.py
@@ -1,9 +1,14 @@
+from kivy.uix import *
+
+
 def camera_control_callback(button):
-    mainbutton.parent.parent
+    button.parent.parent
     print 'Camera Control is pressed'
+
 
 def movement_control_callback(button):
     print 'Movement Control is pressed'
+
 
 def setting_callback(button):
     print 'Setting is pressed'
