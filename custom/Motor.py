d2d59876a0a2ee9c93db85827b7abd2c05ec97f0
huweius <huwilly.sh@gmail.com>
2016-03-31 21:40:10 -0400

    Initial Commit

--- custom/Motor.py
+++ custom/Motor.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as io
-io.setmode(io.BCM)
+
 
 class Motor(object):
     def __init__(self, positive_pin, negative_pin):
@@ -9,15 +9,21 @@ class Motor(object):
         self.positive_pin = positive_pin
         self.negative_pin = negative_pin
 
-        io.setup(self.positive_pin, io.OUT)
-        io.setup(self.negative_pin, io.OUT)
+        if self.positive_pin != self.negative_pin:
+            io.setup(self.positive_pin, io.OUT)
+            io.setup(self.negative_pin, io.OUT)
 
+    def clockwise(self, button):
+        if self.positive_pin != self.negative_pin:
+            io.output(self.positive_pin, True)
+            io.output(self.negative_pin, False)
 
-    def clockwise(self):
-        io.output(self.positive_pin, True)
-        io.output(self.negative_pin, False)
+    def counter_clockwise(self, button):
+        if self.positive_pin != self.negative_pin:
+            io.output(self.positive_pin, False)
+            io.output(self.negative_pin, True)
 
-
-    def counter_clockwise(self):
-        io.output(self.positive_pin, False)
-        io.output(self.negative_pin, True)
+    def stop(self, button):
+        if self.positive_pin != self.negative_pin:
+            io.output(self.positive_pin, False)
+            io.output(self.negative_pin, False)
